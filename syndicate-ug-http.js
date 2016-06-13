c521928b40b7672a495d4aef388ef06ac43e2433
Illyoung Choi <iychoi@email.arizona.edu>
2016-06-13 15:55:57 -0700

    Remove multi-process mode

--- syndicate-ug-http.js
+++ syndicate-ug-http.js
@@ -17,71 +17,62 @@
 
 var rest = require('./rest.js');
 var utils = require('./utils.js');
-var cluster = require('cluster');
 var express = require('express');
 var nodeCache = require('node-cache')
 var app = express();
 
 (function main() {
-    if (cluster.isMaster) {
-        console.log("Syndicate-UG-HTTP start");
-        var cpuCount = require('os').cpus().length;
+    console.log("Syndicate-UG-HTTP start");
 
-        //for(var i=0;i<cpuCount;i++){
-            cluster.fork();
-        //}
-    } else {
-        var args = process.argv.slice(1);
-        var param = utils.parse_args(args);
+    var args = process.argv.slice(1);
+    var param = utils.parse_args(args);
 
-        try {
-            // start restfs
-            app.use(function(req, res, next) {
-                console.log('%s %s', req.method, req.url);
-                next();
-            });
+    try {
+        // start rest
+        app.use(function(req, res, next) {
+            console.log('%s %s', req.method, req.url);
+            next();
+        });
 
-            var ug = rest.init(param);
-            var rfdCache = new nodeCache({
-                stdTTL: 600,
-                checkperiod: 600,
-                useClones: false
-            });
+        var ug = rest.init(param);
+        var rfdCache = new nodeCache({
+            stdTTL: 600,
+            checkperiod: 600,
+            useClones: false
+        });
 
-            rfdCache.on("expired", function(key, value) {
-                console.log("closing expired file handle for read - " + key);
-                rest.safeclose(ug, value);
-            });
+        rfdCache.on("expired", function(key, value) {
+            console.log("closing expired file handle for read - " + key);
+            rest.safeclose(ug, value);
+        });
 
-            var wfdCache = new nodeCache({
-                stdTTL: 3600,
-                checkperiod: 600,
-                useClones: false
-            });
+        var wfdCache = new nodeCache({
+            stdTTL: 3600,
+            checkperiod: 600,
+            useClones: false
+        });
 
-            wfdCache.on("expired", function(key, value) {
-                console.log("closing expired file handle for read - " + key);
-                rest.safeclose(ug, value);
-            });
+        wfdCache.on("expired", function(key, value) {
+            console.log("closing expired file handle for read - " + key);
+            rest.safeclose(ug, value);
+        });
 
-            app.use(function(req, res, next) {
-                req.ug = ug;
-                req.rfdCache = rfdCache;
-                req.wfdCache = wfdCache;
-                next();
-            });
+        app.use(function(req, res, next) {
+            req.ug = ug;
+            req.rfdCache = rfdCache;
+            req.wfdCache = wfdCache;
+            next();
+        });
 
-            app.use('/', rest.getRouter());
+        app.use('/', rest.getRouter());
 
-            app.listen(param.port, function() {
-                console.log("listening at " + param.port);
-                console.log("Worker " + cluster.worker.id + " running");
-            });
+        app.listen(param.port, function() {
+            console.log("listening at " + param.port);
+        });
 
-            // must not shutdown here!
-            //rest.shutdown(ug);
-        } catch (e) {
-            console.log("Exception occured: " + e);
-        }
+        // must not shutdown here!
+        //rest.shutdown(ug);
+    } catch (e) {
+        console.log("Exception occured: " + e);
     }
 })();

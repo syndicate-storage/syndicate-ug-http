d258fe216903705c97eb2dab12977be5de7ae38a
Illyoung Choi <iychoi@email.arizona.edu>
2016-12-12 11:31:00 -0700

    Change license from princeton to arizona

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015 The Trustees of Princeton University
+   Copyright 2016 The Trustees of University of Arizona
 
    Licensed under the Apache License, Version 2.0 (the "License" );
    you may not use this file except in compliance with the License.
